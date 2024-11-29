1b25030718b8dd903cedeb8649844476e34e0cd6
Carlos de Jesús Hernández García <93212816+CarlosHG04@users.noreply.github.com>
2024-11-28 23:52:35 -0600

    3ra carga

---
+++ Ejercico 3.py
@@ -0,0 +1,32 @@
+print("-------------------------------------------------------")
+# Actividad 1
+
+# Usamos un ciclo for para repetir 5 veces
+for _ in range(5):
+    print("Python")
+
+
+
+print("-------------------------------------------------------")
+# Actividad 2
+
+# Solicitamos un valor al usuario
+limite = int(input("Ingresa un valor: "))
+
+# Usamos un ciclo for para generar los números del 1 al valor ingresado
+for numero in range(1, limite + 1):
+    print(numero)
+
+
+print("-------------------------------------------------------")
+# Actividad 3
+
+# Solicitamos un valor al usuario
+limite = int(input("Ingresa un valor: "))
+
+# Usamos un ciclo for para mostrar solo los números pares
+for numero in range(0, limite + 1, 2):
+    print(numero)
+
+
+print("-------------------------------------------------------")
