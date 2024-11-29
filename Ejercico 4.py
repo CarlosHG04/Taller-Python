64e2ebb06e0e9ee9e72c44ca8c683fbd0ad963a7
Carlos de Jesús Hernández García <93212816+CarlosHG04@users.noreply.github.com>
2024-11-28 23:56:25 -0600

    4to cambio

---
+++ Ejercico 4.py
@@ -0,0 +1,27 @@
+print("-------------------------------------------------------")
+# Actividad 1
+
+mi_lista = [10, 20, 30, 40, 50]
+mi_tupla = (100, 200, 300, 400, 500)
+mi_conjunto = {1000, 2000, 3000, 4000, 5000}
+
+print("Lista, primer elemento:", mi_lista[0])  # Acceso por índice
+
+print("Tupla, segundo elemento:", mi_tupla[1])  # Acceso por índice
+
+print("Conjunto, elementos:")
+for elemento in mi_conjunto:
+    print(elemento)
+
+
+print("-------------------------------------------------------")
+# Actividad 2
+
+mi_lista = ["Python", "Java", "C++", "JavaScript", "Ruby"]
+
+print("Primer valor:", mi_lista[0])  
+print("Último valor:", mi_lista[-1])  
+print("Valores del 2 al 4:", mi_lista[1:4])  
+
+
+print("-------------------------------------------------------")
