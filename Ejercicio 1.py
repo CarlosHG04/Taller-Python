e1f1fab58050bdf52d0eb8f222b127e5c279668a
Carlos de Jesús Hernández García <93212816+CarlosHG04@users.noreply.github.com>
2024-11-28 23:37:52 -0600

    Primer ejercicio

---
+++ Ejercicio 1.py
@@ -0,0 +1,44 @@
+# EJERCICIO 1
+# Actividad 1
+var1 = 10
+var2 = 20
+
+print(var1)
+print(var2)
+print("-------------------------------------------------------")
+
+
+# Actividad 2
+
+texto1 = "Hola"
+texto2 = "Mundo"
+
+print(texto1 + " " + texto2)
+
+
+
+print("-------------------------------------------------------")
+
+# Actividad 3
+
+
+# Definicion de vriables
+num1 = 5
+num2 = 15
+
+# Impresion
+print("Antes de modificar:")
+print(num1)
+print(num2)
+
+# Modificacion
+num1 = 50
+
+# Imprecion de las variables después de la modificación
+print("Después de modificar:")
+print(num1)
+print(num2)
+
+
+
+print("-------------------------------------------------------")
